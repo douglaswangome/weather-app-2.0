e58204562b19f74d207df8ff7bf0da1ec53ceb11
Douglas Wangome <ishdagnesh@gmail.com>
2023-06-27 11:48:25 +0300

    Update dependecies

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
-import toast from "react-hot-toast";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss("./tailwind.config.js"), toast()],
+  plugins: [react(), tailwindcss("./tailwind.config.js")],
 });
