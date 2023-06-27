049fc6ccd4754136182d9c1a549dc6c2b73c102d
Douglas Wangome <ishdagnesh@gmail.com>
2023-06-27 17:50:18 +0300

    Remove dynamic background on Home Page

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -12,9 +12,15 @@ const Home = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   // variables
-  const [advice, setAdvice] = useState("Loading");
-  const [bgImage, setBgImage] = useState("url('/images/sunny.jpg')");
-  const [textColor, setTextColor] = useState("black");
+  const [main, setMain] = useState({
+    advice: "Loading",
+    bgImage: "url('/images/sunny.jpg')",
+    color: "black",
+  });
+  const [rest, setRest] = useState({
+    advice: "Loading",
+    bgImage: "url('/images/sunny.jpg')",
+  });
   const location = useLocation();
   const [fullScreenMode, setFullScreenMode] = useState(false);
 
@@ -35,36 +41,152 @@ const Home = () => {
   useEffect(() => {
     // Give Advice
     const giveAdviceAndBackground = (weather_main, weather_desc) => {
-      if (
-        weather_main.toLowerCase() === "rain" ||
-        weather_main.toLowerCase() === "thunderstorm" ||
-        weather_main.toLowerCase() === "drizzle"
-      ) {
-        setBgImage("url('/images/rainy.jpg')");
-        setTextColor("white");
-        if (weather_desc.split(" ")[0] === "light") {
-          setAdvice("Carry an umbrella");
-        } else {
-          setAdvice("Try work from home. Keep Warm");
-        }
-      } else if (weather_main.toLowerCase() === "clouds") {
-        setBgImage("url('/images/cloudy.jpg')");
-        if (
-          weather_desc.split(" ")[0] === "overcast" ||
-          weather_desc.split(" ")[0] === "broken"
+      if (index === 0) {
+        // Adjust main object
+        if (weather_main.toLowerCase() === "atmosphere") {
+          setMain((prevMain) => {
+            return {
+              ...prevMain,
+              bgImage: "url('/images/mist.jpg')",
+            };
+          });
+        } else if (weather_main.toLowerCase() === "snow") {
+          setMain((prevMain) => {
+            return {
+              ...prevMain,
+              advice: "Just stay indoors and enjoy your favorite movie",
+            };
+          });
+        } else if (weather_main.toLowerCase() === "clouds") {
+          setMain((prevMain) => {
+            return {
+              ...prevMain,
+              bgImage: "url('/images/cloudy.jpg')",
+            };
+          });
+          if (
+            weather_desc.split(" ")[0] === "overcast" ||
+            weather_desc.split(" ")[0] === "broken"
+          ) {
+            setMain((prevMain) => {
+              return {
+                ...prevMain,
+                advice: "Might be chilly, wear warm clothing",
+              };
+            });
+          } else {
+            setMain((prevMain) => {
+              return { ...prevMain, advice: "Just a normal day" };
+            });
+          }
+        } else if (
+          weather_main.toLowerCase() === "rain" ||
+          weather_main.toLowerCase() === "thunderstorm" ||
+          weather_main.toLowerCase() === "drizzle"
         ) {
-          setAdvice("Might be chilly, wear warm clothing");
+          setMain((prevMain) => {
+            return {
+              ...prevMain,
+              color: "white",
+              bgImage: "url('/images/rainy.jpg')",
+            };
+          });
+          if (weather_desc.split(" ")[0].toLowerCase() === "light") {
+            setMain((prevMain) => {
+              return {
+                ...prevMain,
+                advice: "Carry an umbrella",
+              };
+            });
+          } else {
+            setMain((prevMain) => {
+              return {
+                ...prevMain,
+                advice: "Try work from home. Keep Warm",
+              };
+            });
+          }
         } else {
-          setAdvice("Just a normal day");
+          setMain((prevMain) => {
+            return {
+              ...prevMain,
+              advice: "Take a run or do a fun activity",
+            };
+          });
         }
-      } else if (weather_main.toLowerCase() === "snow") {
-        setTextColor("white");
-        setAdvice("Just stay indoors and enjoy your favorite movie");
-        setBgImage("url('/images/snow.jpg')");
-      } else if (weather_main.toLowerCase() === "atmosphere") {
-        setBgImage("url('/images/mist.jpg')");
       } else {
-        setAdvice("Get out and have fun or walk about");
+        // Adjust rest object
+        if (weather_main.toLowerCase() === "atmosphere") {
+          setRest((prevRest) => {
+            return {
+              ...prevRest,
+              bgImage: "url('/images/mist.jpg')",
+            };
+          });
+        } else if (weather_main.toLowerCase() === "snow") {
+          setRest((prevRest) => {
+            return {
+              ...prevRest,
+              advice: "Just stay indoors and enjoy your favorite movie",
+            };
+          });
+        } else if (weather_main.toLowerCase() === "clouds") {
+          setRest((prevRest) => {
+            return {
+              ...prevRest,
+              bgImage: "url('/images/cloudy.jpg')",
+            };
+          });
+          if (
+            weather_desc.split(" ")[0] === "overcast" ||
+            weather_desc.split(" ")[0] === "broken"
+          ) {
+            setRest((prevRest) => {
+              return {
+                ...prevRest,
+                advice: "Might be chilly, wear warm clothing",
+              };
+            });
+          } else {
+            setRest((prevRest) => {
+              return { ...prevRest, advice: "Just a normal day" };
+            });
+          }
+        } else if (
+          weather_main.toLowerCase() === "rain" ||
+          weather_main.toLowerCase() === "thunderstorm" ||
+          weather_main.toLowerCase() === "drizzle"
+        ) {
+          setRest((prevRest) => {
+            return {
+              ...prevRest,
+              color: "white",
+              bgImage: "url('/images/rainy.jpg')",
+            };
+          });
+          if (weather_desc.split(" ")[0].toLowerCase() === "light") {
+            setRest((prevRest) => {
+              return {
+                ...prevRest,
+                advice: "Carry an umbrella",
+              };
+            });
+          } else {
+            setRest((prevRest) => {
+              return {
+                ...prevRest,
+                advice: "Try work from home. Keep Warm",
+              };
+            });
+          }
+        } else {
+          setRest((prevRest) => {
+            return {
+              ...prevRest,
+              advice: "Take a run or do a fun activity",
+            };
+          });
+        }
       }
     };
     // Fetch Data and Assign Location
@@ -95,7 +217,7 @@ const Home = () => {
   return (
     <div
       className="h-screen overflow-hidden !bg-cover !bg-no-repeat"
-      style={{ background: bgImage }}
+      style={{ background: main.bgImage }}
     >
       <div className="relative flex justify-center py-3 text-4xl bg-transparent shadow-md font-pacifico ">
         <span className="text-sky-500">Weather App</span>
@@ -130,9 +252,9 @@ const Home = () => {
             <div className="flex flex-col py-0">
               <div className="w-full py-3 text-4xl">
                 <span
-                  className={`font-pacifico font-extralight text-${textColor}`}
+                  className={`font-pacifico font-extralight text-${main.color}`}
                 >
-                  {advice}
+                  {main.advice}
                 </span>
               </div>
               <div className="flex flex-col pb-4 overflow-x-scroll">
@@ -148,7 +270,7 @@ const Home = () => {
                   ))}
                 </div>
               </div>
-              <div className={`text-xl text-${textColor}`}>
+              <div className={`text-xl text-${main.color}`}>
                 <span className="before:content-['~']">
                   The background image may not match your current sky view
                 </span>
@@ -156,8 +278,8 @@ const Home = () => {
             </div>
           </div>
           <Rest
-            advice={advice}
-            bgImage={bgImage}
+            advice={rest.advice}
+            bgImage={rest.bgImage}
             city={city}
             data={list[index]}
             onclick={toggleFullScreen}
